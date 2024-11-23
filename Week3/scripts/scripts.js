b720df017c970e564f46bc139a61c2e14ef80bd7
DiNobrega <81649638+DiNobrega@users.noreply.github.com>
2024-11-23 18:51:34 -0300

    Adicionando arquivos iniciais do projeto

---
+++ Week3/scripts/scripts.js
@@ -0,0 +1,6 @@
+
+document.querySelectorAll('.cta-button').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('You clicked a button! This can be linked to booking functionality.');
+    });
+});
